16a994deeeb680a2e96a64e95ffdefe5b5aaf0d2
richicoder <richicoder@gmail.com>
2026-02-17 09:13:43 -0400

    sitemap and additions indications

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com', // replace with your actual site URL
   server: { open: import.meta.env.DEV, host: import.meta.env.DEV },
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
